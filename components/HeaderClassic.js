e5ccb8c5c468871606e23baba7a0514c64a94c1c
Yahya Makarim <ymakarim@gmail.com>
2021-07-07 21:23:03 +0430

    headerClassic done

--- components/HeaderClassic.js
+++ components/HeaderClassic.js
@@ -30,7 +30,7 @@ function HeaderClassic({ router }) {
 
   return (
     <>
-      <header className='max-w-screen-2xl mx-auto p-5 pb-0 sm:mt-4 flex items-end justify-between relative'>
+      <header className='max-w-screen-2xl mx-auto p-5 pb-0 sm:mt-4 flex items-center justify-between relative'>
         <ImageComponentity
           src='https://iap.af/wp-content/uploads/2021/06/IAP-Landscape-Transparent.png'
           classes='h-12 w-52 sm:h-16 sm:w-72'
@@ -138,32 +138,23 @@ function HeaderClassic({ router }) {
           </div>
         </div>
 
-        <div className='flex flex-col gap-2 items-stretch justify-center'>
-          <div className='text-xs text-white bg-gray-600 px-3 py-2 hidden lg:flex justify-between items-center'>
+        <div className='hidden lg:flex flex-col gap-2 items-center justify-between'>
+          <div className='text-gray-600 font-semibold py-2 hidden lg:block'>
             <Link href='https://iap.af/wp-content/uploads/2021/06/2021-Corporate-Profile.pdf'>
-              <a aria-label='corporate profile' className='underline'>
+              <a aria-label='corporate profile' className='hover:underline'>
                 Download Our Corporate profile
               </a>
             </Link>
-            <span>|</span>
-            <span>
-              Have any questions?{' '}
-              <Link href='tel:+93729444427'>
-                <a aria-label='telephone' className='underline'>
-                  0729 4444 27
-                </a>
-              </Link>
-            </span>
           </div>
-          <ul className='hidden lg:flex flex-row gap-1'>
+          <ul className='flex flex-row gap-1 border-t'>
             {navs.map((nav, index) => {
               return nav.href != '#' ? (
                 <li className='flex items-center justify-center' key={index}>
                   <Link href={nav.href}>
                     <a
-                      className={`py-2 px-5 text-sm font-semibold border-gray-600 border-0 hover:border-b-4 focus:outline-none ${
+                      className={`py-2 px-5 font-semibold border-gray-600 border-0 hover:border-b-4 focus:outline-none ${
                         router.pathname == nav.href
-                          ? 'border-b-4 border-gray-600 hover:text-gray-700'
+                          ? 'border-t-4 border-gray-600 hover:text-gray-700'
                           : 'text-gray-700 hover:text-gray-500'
                       }`}
                       aria-label='nav link'
@@ -177,7 +168,7 @@ function HeaderClassic({ router }) {
                   key={index}
                   className='dropdown flex items-center justify-center inline-block relative group'
                 >
-                  <button className='block text-sm px-5 py-2 font-semibold text-gray-700 hover:text-gray-500 flex gap-1'>
+                  <button className='block px-5 py-2 font-semibold text-gray-700 hover:text-gray-500 flex items-center gap-1'>
                     <span className='mr-1'>{nav.text}</span>
                     <svg
                       className='fill-current h-4 w-4'
@@ -187,7 +178,7 @@ function HeaderClassic({ router }) {
                       <path d='M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z' />{' '}
                     </svg>
                   </button>
-                  <ul className='dropdown-menu hidden group-hover:block absolute left-0 top-0 bg-gray-600 text-white mt-10'>
+                  <ul className='dropdown-menu hidden group-hover:block absolute left-0 top-0 bg-gray-600 text-white mt-10 z-10'>
                     {nav.dropdown.map((nav, index) => {
                       return (
                         <li key={index}>
@@ -211,16 +202,34 @@ function HeaderClassic({ router }) {
               )
             })}
           </ul>
-          <span className='flex items-center justify-center gap-1'>
-            <button
-              aria-label='navbar-mobile'
-              onClick={() => setDisplay(!display)}
-              className='md:hidden z-10 whitespace-no-wrap w-12 h-12 flex items-center justify-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-500 focus:outline-none focus:border-gray-700 focus:shadow-outline-gray active:bg-gray-700 transition ease-in-out duration-150'
-            >
-              {display ? <SVGCross /> : <SVGBurgernav />}
-            </button>
-          </span>
         </div>
+
+        <div className='h-16 w-72 hidden lg:flex items-center'>
+          <span className='mr-2'>
+            <ImageComponentity
+              src='https://iapcorporate.com/wp-content/uploads/2018/08/conversations-4872_af60b258-251e-41af-b238-dfb706d7b3d4.svg'
+              classes={'w-12 h-12'}
+            />
+          </span>
+          <div className='flex flex-col items-center'>
+            <span className='mb-1 font-semibold text-gray-600'>Have any questions?</span>
+            <Link href='tel:+93729444427'>
+              <a aria-label='telephone' className='hover:underline'>
+                <b>Call:</b> 0729 4444 27
+              </a>
+            </Link>
+          </div>
+        </div>
+
+        <span className='lg:hidden z-10 flex items-center justify-center gap-1'>
+          <button
+            aria-label='navbar-mobile'
+            onClick={() => setDisplay(!display)}
+            className='whitespace-no-wrap w-12 h-12 flex items-center justify-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-500 focus:outline-none focus:border-gray-700 focus:shadow-outline-gray active:bg-gray-700 transition ease-in-out duration-150'
+          >
+            {display ? <SVGCross /> : <SVGBurgernav />}
+          </button>
+        </span>
       </header>
     </>
   )
