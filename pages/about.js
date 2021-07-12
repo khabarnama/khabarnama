8e26767d36cd05df0acfb070ff862230148af0c4
Yahya Makarim <ymakarim@gmail.com>
2021-07-12 10:22:23 +0430

    about grid really done

--- pages/about.js
+++ pages/about.js
@@ -38,7 +38,7 @@ function About({
       <Head>{ReactHtmlParser(page.yoast_head)}</Head>
       <div className='max-w-screen-xl mx-auto my-8 relative'>
         <div className='grid grid-cols-1 lg:grid-cols-12 items-start gap-8 relative'>
-          <div className='hidden lg:block col-span-1 lg:col-span-3 sticky top-0'>
+          <div className='hidden lg:inline-block col-span-1 lg:col-span-3 sticky top-0'>
             <div className='border bg-white p-8 mb-8'>
               <h3 className='mb-3 text-3xl block uppercase text-gray-900 font-bold'>
                 {topLeft.name}
@@ -60,7 +60,7 @@ function About({
                 })}
               </ul>
             </div>
-            <div className='border bg-white p-8'>
+            <div className='border bg-white p-8 mb-8'>
               <h3 className='mb-3 text-3xl block uppercase text-gray-900 font-bold'>
                 {topLeft.name}
               </h3>
