d878c46f35434edea511e0ae4a735b7f09c2df07
IAPCorporate <62869859+IAPCorporate@users.noreply.github.com>
2021-07-13 18:53:19 +0430

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,6 +6,6 @@ module.exports = withPWA({
     disable: process.env.NODE_ENV === 'development'
   },
   images: {
-    domains: ['iap.af', 'iapcorporate.com']
+    domains: ['admin.iap.af']
   }
 })
