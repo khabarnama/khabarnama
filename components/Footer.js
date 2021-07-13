b11d4764931f5a001d37d7ee8f655a175a830b63
Mr. Code (@mymakarim) <ymakarim@gmail.com>
2021-07-13 21:45:53 +0430

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -11,13 +11,14 @@ export default function Footer({ topLeft, topRight, topRight2 }) {
               <Link
                 key={item.id}
                 href={`${
-                  item.url.startsWith('http') || item.url.startsWith('mailto') || item.url.startsWith('tel')
+                  item.url.startsWith('mailto') || item.url.startsWith('tel')
                     ? item.url
                     : '/${item.url.replace("https://admin.iap.af/", "")}'
                 }`}
               >
                 <a
                   target={`${item.url.startsWith('http') || item.url.startsWith('mailto') || item.url.startsWith('tel') ? '_blank' : '_self'}`}
+                  rel="noreferrer"
                   aria-label='Footer Link'
                   className='my-1 block text-gray-300 font-medium hover:text-white'
                 >
@@ -36,13 +37,14 @@ export default function Footer({ topLeft, topRight, topRight2 }) {
               <Link
                 key={item.id}
                 href={`${
-                  item.url.startsWith('http') || item.url.startsWith('mailto') || item.url.startsWith('tel')
+                  item.url.startsWith('mailto') || item.url.startsWith('tel')
                     ? item.url
                     : '/${item.url.replace("https://admin.iap.af/", "")}'
                 }`}
               >
                 <a
                   target={`${item.url.startsWith('http') || item.url.startsWith('mailto') || item.url.startsWith('tel') ? '_blank' : '_self'}`}
+                  rel="noreferrer"
                   aria-label='Footer Link'
                   className='my-1 block text-gray-300 font-medium hover:text-white'
                 >
@@ -61,13 +63,14 @@ export default function Footer({ topLeft, topRight, topRight2 }) {
               <Link
                 key={item.id}
                 href={`${
-                  item.url.startsWith('http') || item.url.startsWith('mailto') || item.url.startsWith('tel')
+                  item.url.startsWith('mailto') || item.url.startsWith('tel')
                     ? item.url
                     : '/${item.url.replace("https://admin.iap.af/", "")}'
                 }`}
               >
                 <a
                   target={`${item.url.startsWith('http') || item.url.startsWith('mailto') || item.url.startsWith('tel') ? '_blank' : '_self'}`}
+                  rel="noreferrer"
                   aria-label='Footer Link'
                   className='my-1 block text-gray-300 font-medium hover:text-white'
                 >
