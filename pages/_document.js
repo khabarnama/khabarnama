71496efc582369e0b0a5fb78623e2480449c7c94
Mr. Code (@mymakarim) <ymakarim@gmail.com>
2021-07-14 08:36:39 +0400

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -14,11 +14,11 @@ class MyDocument extends Document {
           <link rel='preconnect' href='https://fonts.gstatic.com' />
           <link href='https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap' />
 
-          <meta name='application-name' content='Componentity App' />
+          <meta name='application-name' content='IAP' />
           <meta name='apple-mobile-web-app-capable' content='yes' />
           <meta name='apple-mobile-web-app-status-bar-style' content='default' />
-          <meta name='apple-mobile-web-app-title' content='Componentity App' />
-          <meta name='description' content='Best Componentity PWA App in the world' />
+          <meta name='apple-mobile-web-app-title' content='IAP' />
+          <meta name='description' content='Intellectual Applications and Products' />
           <meta name='format-detection' content='telephone=no' />
           <meta name='mobile-web-app-capable' content='yes' />
           <meta name='msapplication-config' content='/static/icons/browserconfig.xml' />
@@ -26,9 +26,9 @@ class MyDocument extends Document {
           <meta name='msapplication-tap-highlight' content='no' />
           <meta name='theme-color' content='#000000' />
 
-          <link rel='apple-touch-icon' href='/icons/apple-touch-icon.png' />
+          <link rel='apple-touch-icon' href='https://admin.iap.af/wp-content/uploads/2021/06/cropped-IAP-Square-Initials-scaled-1-192x192.jpg' />
           <link rel='manifest' href='/manifest.json' />
-          <link rel='shortcut icon' href='/favicon.ico' />
+          <link rel='shortcut icon' href='https://admin.iap.af/wp-content/uploads/2021/06/cropped-IAP-Square-Initials-scaled-1-192x192.jpg' />
         </Head>
         <body>
           <Main />
