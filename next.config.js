6ec155387fbced32cb50472e4fe73403b5ed1e40
Yahya Makarim <ymakarim@gmail.com>
2021-07-26 16:34:38 +0430

    static js files links

--- next.config.js
+++ next.config.js
@@ -10,16 +10,16 @@ module.exports = withPWA({
     domains: ['admin.iap.af']
   },
   env: {
-    dialog: fs.readFileSync('./js/dialog.js').toString(),
-    elementorwebpackruntime: fs.readFileSync('./js/elementorwebpackruntime.js').toString(),
-    frontend: fs.readFileSync('./js/frontend.js').toString(),
-    jquery: fs.readFileSync('./js/jquery.js').toString(),
-    jquerycore: fs.readFileSync('./js/jquerycore.js').toString(),
-    jquerymigrate: fs.readFileSync('./js/jquerymigrate.js').toString(),
-    preloadedmodules: fs.readFileSync('./js/preloadedmodules.js').toString(),
-    sharelink: fs.readFileSync('./js/sharelink.js').toString(),
-    swiper: fs.readFileSync('./js/swiper.js').toString(),
-    waypoints: fs.readFileSync('./js/waypoints.js').toString(),
-    frontendmodules: fs.readFileSync('./js/frontendmodules.js').toString()
+    dialog: fs.readFileSync('/js/dialog.js').toString(),
+    elementorwebpackruntime: fs.readFileSync('/js/elementorwebpackruntime.js').toString(),
+    frontend: fs.readFileSync('/js/frontend.js').toString(),
+    jquery: fs.readFileSync('/js/jquery.js').toString(),
+    jquerycore: fs.readFileSync('/js/jquerycore.js').toString(),
+    jquerymigrate: fs.readFileSync('/js/jquerymigrate.js').toString(),
+    preloadedmodules: fs.readFileSync('/js/preloadedmodules.js').toString(),
+    sharelink: fs.readFileSync('/js/sharelink.js').toString(),
+    swiper: fs.readFileSync('/js/swiper.js').toString(),
+    waypoints: fs.readFileSync('/js/waypoints.js').toString(),
+    frontendmodules: fs.readFileSync('/js/frontendmodules.js').toString()
   }
 })
